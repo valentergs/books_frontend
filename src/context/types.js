cf962555752062a7ddf424fff7d2589c7fe9d51e
Rodrigo Valente <rodrigovalente@hotmail.com>
2019-12-16 18:43:49 -0300

    first commit

---
+++ src/context/types.js
@@ -0,0 +1,5 @@
+// Livro
+export const TODOS_LIVROS = "TODOS_LIVROS";
+export const INSERIR_LIVRO = "INSERIR_LIVRO";
+export const DELETAR_LIVRO = "DELETAR_LIVRO";
+export const EDITAR_LIVRO = "EDITAR_LIVRO";
